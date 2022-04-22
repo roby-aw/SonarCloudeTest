c37a9cf1856df601359cd4d357405ff09fc93d1f
Roby Awaluddin <61074887+roby-aw@users.noreply.github.com>
2022-04-22 15:06:07 +0700

    Update test.go

--- test.go
+++ test.go
@@ -1,12 +1,14 @@
 package main
 
+benar := true
+
 func passed(score int) bool {
   switch {
   case score > 80:
-    return true
+    return benar
   case score > 70:
-    return true
+    return benar
   default:
-    return true
+    return benar
   }
 }
